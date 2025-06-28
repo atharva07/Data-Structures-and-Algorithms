1f87644a5d3e9323a219f04bd3cda2d28bbaa39c
atharva-predigle <atharva.hiwase07@gmail.com>
2025-06-28 18:03:37 +0530

    Majority Element

---
+++ DataStructure/Arrays/MajorityElementCount3Times.java
@@ -0,0 +1,64 @@
+package DataStructure.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorityElementCount3Times {
+    
+    public static List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        // First pass to find potential candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        // second pass to verify the candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        int n = nums.length;
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > n / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 3, 1, 2, 3, 3, 4, 2, 2};
+        List<Integer> res = majorityElement(arr);
+        
+        System.out.println(res);
+    }
+}
