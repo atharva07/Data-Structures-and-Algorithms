e2446257040956bed6ae84b065c4700f72afb656
atharva-predigle <atharva.hiwase07@gmail.com>
2025-07-05 16:05:30 +0530

    4 sum equal to target

---
+++ DataStructure/Arrays/FourSumEqualToTarget.java
@@ -0,0 +1,59 @@
+package DataStructure.Arrays;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSumEqualToTarget {
+    
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n -2; j++) {
+                if (j > i && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+                int remainingTaget = target - nums[i] - nums[j];
+
+                while (left < right) {
+                    int currentSum = nums[left] + nums[right];
+                    if (currentSum == remainingTaget) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (currentSum < remainingTaget) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        List<List<Integer>> res = new ArrayList<>();
+        res = fourSum(arr, target);
+        System.out.println(res);
+    }
+} 
