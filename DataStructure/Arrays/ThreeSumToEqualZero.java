2ef178237b02210d90eec90ad2fcf9dc2d75011f
atharva-predigle <atharva.hiwase07@gmail.com>
2025-07-05 12:28:55 +0530

    Three Sum Problem

---
+++ DataStructure/Arrays/ThreeSumToEqualZero.java
@@ -0,0 +1,54 @@
+package DataStructure.Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSumToEqualZero {
+    
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int i = 0; i < n-2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // skip duplicate element for i
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int currentSum = nums[left] + nums[right];
+                if (currentSum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (currentSum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result; 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1,0,1,2,-1,-4};
+        List<List<Integer>> res = new ArrayList<>();
+        res = threeSum(arr);
+        System.out.println(res);
+    }
+}
