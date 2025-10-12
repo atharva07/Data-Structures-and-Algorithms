7165ae798172ea526c25693c95ac36503c05be59
atharva07 <atharva.hiwase07@gmail.com>
2025-10-12 18:27:27 +0530

    koko eating bananas

---
+++ Algorithms/Searching/BinarySearch/BinarySearchOnAnswers/KokoEatsBananas/KokoEatingBananaBrute.java
@@ -0,0 +1,45 @@
+package Algorithms.Searching.BinarySearch.BinarySearchOnAnswers.KokoEatsBananas;
+
+public class KokoEatingBananaBrute {
+    
+    public static int findMax(int[] v) {
+        int maxi = Integer.MIN_VALUE;
+        int n = v.length;
+        // find the maximum
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, v[i]);
+        }
+        return maxi;
+    }
+
+    public static int calculateTotalHours(int[] v, int hourly) {
+        int totalH= 0;
+        int n = v.length;
+
+        // find total hours
+        for (int i = 0; i < n; i++) {
+            totalH += Math.ceil((double)(v[i])/ (double)(hourly));
+        }
+        return totalH;
+    }
+
+    public static int minimumRateToEatBananas(int[] v, int h) {
+        int maxi = findMax(v);
+
+        // find minimum vlaue of k
+        for (int i = 1; i <= maxi; i++) {
+            int reqTime = calculateTotalHours(v, h);
+            if (reqTime <= h) {
+                return i;
+            }
+        }
+        return maxi;
+    }
+
+    public static void main(String[] args) {
+        int[] v = {7, 15, 6, 3};
+        int h = 8;
+        int ans = minimumRateToEatBananas(v, h);
+        System.out.println("Koko should eat at least " + ans + " bananas/hr.");
+    }
+}
