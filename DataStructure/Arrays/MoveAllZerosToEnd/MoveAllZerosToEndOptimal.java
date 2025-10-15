d8d36dee289938462d53463930ed408e84250f29
atharva07 <atharva.hiwase07@gmail.com>
2025-10-15 23:12:48 +0530

    Move zeros to end optimized

--- DataStructure/Arrays/MoveAllZerosToEnd/MoveAllZerosToEndOptimal.java
+++ DataStructure/Arrays/MoveAllZerosToEnd/MoveAllZerosToEndOptimal.java
@@ -1,10 +1,10 @@
 package DataStructure.Arrays.MoveAllZerosToEnd;
 
 public class MoveAllZerosToEndOptimal {
-    
+    // this is a two pointer approach
     public static int[] moveZerosToEnd(int[] arr, int n) {
         int j = -1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // --- O(N)
             if (arr[i] == 0) {
                 j = i;
                 break;
@@ -13,7 +13,7 @@ public class MoveAllZerosToEndOptimal {
 
         if (j == -1) return arr;
 
-        for (int i = j + 1; i < n; i++) {
+        for (int i = j + 1; i < n; i++) { // --- O(N)
             if (arr[i] != 0) {
                 int temp = arr[i];
                 arr[i] = arr[j];
@@ -23,6 +23,9 @@ public class MoveAllZerosToEndOptimal {
         }
         return arr;
     }
+
+    // Time complexity - O(N) - we are using 2 loops but, we are traversing the array once
+    // space complexity - O(1)
 
     public static void main(String[] args) {
         int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
