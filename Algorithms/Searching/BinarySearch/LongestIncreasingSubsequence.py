7c0a86a5c94bbc702af8caf6f495c023f7bcf89d
atharva07 <atharva.hiwase07@gmail.com>
2026-01-26 22:36:09 +0530

    Longest Increasing subsequence

---
+++ Algorithms/Searching/BinarySearch/LongestIncreasingSubsequence.py
@@ -0,0 +1,41 @@
+from typing import List
+
+class LongestIncreasingSubsequence:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        tails = []
+
+        for x in nums:
+            left = 0
+            right = len(tails) - 1
+            pos = len(tails) # default position to insert
+
+            while left <= right:
+                mid = left + (right - left) // 2
+
+                if tails[mid] >= x:
+                    pos = mid
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            if pos == len(tails):
+                tails.append(x)
+            else:
+                tails[pos] = x
+        
+        return len(tails)
+    
+def main():
+    sol = LongestIncreasingSubsequence()
+    nums = [10,9,2,5,3,7,101,18]
+    res = sol.lengthOfLIS(nums)
+    print(res)
+
+if __name__ == "__main__":
+    main()
+            
+
+                
+
+        
+        
