e09e5b63648435f26573a652666bac484b42bc71
atharva07 <atharva.hiwase07@gmail.com>
2026-05-21 02:11:19 +0530

    Product Of Array Except Self

--- PrefixSum/ProductOfArrayExceptSelf.py
+++ PrefixSum/ProductOfArrayExceptSelf.py
@@ -5,21 +5,24 @@ class ProductOfArrayExceptSelf:
         n = len(nums)
         result = [1] * n
 
+        # Calculate the product of all elements to the left of i
         prefix = 1
         for i in range(n):
             result[i] = prefix
             prefix *= nums[i]
 
+        # Calculate the product of all elements to the right of i
+        # Here we go from right to left to calculate the suffix product and multiple it with the prefix product
         suffix = 1
         for i in range(n-1, -1, -1):
-            result[i] = suffix
+            result[i] *= suffix
             suffix *= nums[i]
 
         return result
     
 def main():
     sol = ProductOfArrayExceptSelf()
-    nums = [1,2,4,6]
+    nums = [1,2,3,4]
     res = sol.productExceptSelf(nums)
     print(res)
 
